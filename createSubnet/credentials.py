35ef7f529173e1649bf6bdeb965512e378609f71
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-07 12:52:19 -0500

    Delete credentials.py

--- createSubnet/credentials.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ["ARM_CLIENT_ID"] = "" # redacted
-os.environ["ARM_TENANT_ID"] = ""
-os.environ["ARM_CLIENT_SECRET"] = ""
-os.environ["ARM_SUBSCRIPTION_ID"] = ""
-os.system("terraform plan")
-os.system("terraform apply -auto-approve")
