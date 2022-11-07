f5c1798edd32f1b64a2b34ebf1b0867ff1d9eb66
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-07 12:50:34 -0500

    Create variables.tf

---
+++ createSubnet/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {
+  description = "RG name"
+  type = string
+  default = ""
+}
+
+variable "virtual_network_name" {
+  description = "VN name"
+  type = string
+  default = ""
+}
+
+variable "subnet_name" {
+  description = "Subnet name"
+  type = string
+  default = ""
+}
