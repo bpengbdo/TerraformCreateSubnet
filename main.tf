1aef027a525f55cc5d1774926709d46546ea6c1f
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-07 16:03:43 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-module "create-subnet" {
-  source = "./createSubnet"
+module "create-virtual-network" {
+  source = "./createVN"
 
-  subnet_name          = "terraformSubnet2"
-  resource_group_name = "terraformRG"
   virtual_network_name = "terraformVN"
+  resource_group_name  = "terraformRG"
+  address_space        = ["10.0.0.0/16"]
+  location             = "eastus"
+}
+
+module "create-virtual-network2" {
+  source = "./createVN"
+
+  virtual_network_name = "terraformVN2"
+  resource_group_name  = "terraformRG2"
+  address_space        = ["10.0.0.0/16"]
+  location             = "eastus"
 }
