ce7a7c070eb91672302595012507fd5399746050
bpengbdo <113641312+bpengbdo@users.noreply.github.com>
2022-11-15 10:44:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,3 @@ module "create-virtual-network" {
   address_space        = ["10.0.0.0/16"]
   location             = "eastus"
 }
-
-module "create-virtual-network2" {
-  source = "./createVN"
-
-  virtual_network_name = "terraformVN2"
-  resource_group_name  = "terraformRG2"
-  address_space        = ["10.0.0.0/16"]
-  location             = "eastus"
-}
